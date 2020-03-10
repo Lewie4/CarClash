9f3908e229cc809e35e4236e6df29f99aa456a8b
Glen Walker <glenjwalker@hotmail.com>
2020-03-10 11:37:49 +0000

    starting to add fake opponents

--- Assets/Scripts/TrackTrigger.cs
+++ Assets/Scripts/TrackTrigger.cs
@@ -7,6 +7,8 @@ public class TrackTrigger : MonoBehaviour
 {
     public UnityEvent m_ActionList;
     public Collider m_triggerBox;
+    public bool m_triggered;
+ 
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +20,15 @@ public class TrackTrigger : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!m_triggered)
+        {
+            m_ActionList.Invoke();
+            m_triggered = true;
+        }
+        //Debug.Log("ENTERED" + other.name);
     }
 }
