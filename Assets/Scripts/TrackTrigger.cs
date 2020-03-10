e1a1b76e4be147e524536f4eaf1fabdc0e095b9f
Glen Walker <glenjwalker@hotmail.com>
2020-03-10 12:46:16 +0000

    Adding in more set piece stuff

--- Assets/Scripts/TrackTrigger.cs
+++ Assets/Scripts/TrackTrigger.cs
@@ -6,9 +6,10 @@ using UnityEngine.Events;
 public class TrackTrigger : MonoBehaviour
 {
     public UnityEvent m_ActionList;
+    public UnityEvent m_EndActionList;
     public Collider m_triggerBox;
     public bool m_triggered;
- 
+    public float m_endTimer;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +20,14 @@ public class TrackTrigger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (m_triggered && (m_endTimer > 0f))
+        {
+            m_endTimer -= Time.deltaTime;
+            if(m_endTimer < 0f)
+            {
+                m_EndActionList.Invoke(); 
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
